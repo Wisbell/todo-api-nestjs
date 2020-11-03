ef37cd4a3a0e05667a2e39d6c7febfd1b8d8281f
Westley Isbell <coolwest@gmail,com>
2020-11-02 21:43:07 -0600

    Add todo comments

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  // TODO: Get all todo
+
+  // TODO: Get a todo
+
+  // TODO: Create a todo
+
+  // TODO: Update a todo
+
+  // TODO: Delete a todo
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  // TODO: Get all todo
+
+  // TODO: Get a todo
+
+  // TODO: Create a todo
+
+  // TODO: Update a todo
+
+  // TODO: Delete a todo
+}
