0aff52f8ae223a3345f86f19ecc66ff4affcc4fc
Westley Isbell <coolwest@gmail,com>
2020-11-03 15:22:54 -0600

    Generate user module, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     InMemoryDBModule.forRoot(),
-    TodoModule
+    TodoModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
