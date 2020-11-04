b984168dd454efbbb88bd5bcc97819dfcc8db84a
Westley Isbell <coolwest@gmail,com>
2020-11-04 17:19:16 -0600

    disable todo controller while fixing auth

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -8,29 +8,29 @@ import { TodoService } from './todo.service';
 export class TodoController {
   constructor(private readonly todoService: TodoService) { }
 
-  @Get()
-  getAllTodos(): TodoEntity[] {
-    return this.todoService.getAllTodos();
-  }
+  // @Get()
+  // getAllTodos(): TodoEntity[] {
+  //   return this.todoService.getAllTodos();
+  // }
 
-  @Get(':id')
-  getTodo(@Param('id') id: string): TodoEntity {
-    return this.todoService.getTodo(id);
-  }
+  // @Get(':id')
+  // getTodo(@Param('id') id: string): TodoEntity {
+  //   return this.todoService.getTodo(id);
+  // }
 
-  @Post()
-  createTodo(todo: TodoEntity): TodoEntity {
-    return this.todoService.createTodo(todo);
-  }
+  // @Post()
+  // createTodo(todo: TodoEntity): TodoEntity {
+  //   return this.todoService.createTodo(todo);
+  // }
 
-  @Patch(':id')
-  updateTodo(@Param('id') id: string, updatedTodo: TodoEntity): TodoEntity {
-    this.todoService.updateTodo(updatedTodo);
-    return this.todoService.getTodo(updatedTodo.id);
-  }
+  // @Patch(':id')
+  // updateTodo(@Param('id') id: string, updatedTodo: TodoEntity): TodoEntity {
+  //   this.todoService.updateTodo(updatedTodo);
+  //   return this.todoService.getTodo(updatedTodo.id);
+  // }
 
-  @Delete(':id')
-  deleteTodo(id: string): void {
-    return this.todoService.deleteTodo(id);
-  }
+  // @Delete(':id')
+  // deleteTodo(id: string): void {
+  //   return this.todoService.deleteTodo(id);
+  // }
 }
