e126ea1f5a46b17e1a26e56474c0033deb071c3a
Wiki Wookie <westleyisbell@gmail.com>
2020-11-09 18:51:54 -0600

    Add todo comment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // NOTE: Enable cors is necessary for cross domain requests
+  // TODO: Check if enableCors is necessary when deployed
   app.enableCors(); 
   await app.listen(3000);
 }
