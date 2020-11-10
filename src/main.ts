b15c370243a1bd4ac249827314528045dfb2e0d8
Wiki Wookie <westleyisbell@gmail.com>
2020-11-09 18:51:34 -0600

    Enable cors for local testing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // NOTE: Enable cors is necessary for cross domain requests
+  app.enableCors(); 
   await app.listen(3000);
 }
 bootstrap();
